4df38a88781bc3a5cc6b783341c0f21db94178a4
Onur Timur <onurerinctimur@gmail.com>
2015-11-18 03:17:19 +0200

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+
+# I'll execute my programs, with the input directory tweet_input and output the files in the directory tweet_output
+python ./src/tweet_clean_and_average.py ./tweet_input/tweets.txt ./tweet_output/ft1.txt ./tweet_output/ft2.txt
